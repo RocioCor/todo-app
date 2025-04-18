56212d0355f17fb596711fac0212fd7035b8b501
RocioCor <rocio.corredera2014@gmail.com>
2025-04-18 00:19:26 -0300

    Agregado: almacenamiento de tareas en localStorage

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const addBtn = document.getElementById ('addTaskBtn');
 const input = document.getElementById ('taskInput');
 const taskList = document.getElementById ('taskList');
-
+document.addEventListener('DOMContentLoaded, loadTasks');
 addBtn.assEvenListener ('click', ()=> {
-    const taskText = input.ariaValueMax.trim();
+    const taskText = input.value.trim();
     if (taskText ==="") return;
-    const li =  document.createElement('li');
-    li.textContent = taskText;
-    const delBtn = document.createElement('button');
-    delBtn. textContent = '❌';
-    delBtn.onclick = () => li.remove();
-    li.onclick = () => li.classList.toggle('done')
-    li,appendChild(delBtn);
-    taskList.appendChild(li)
+    addTask(taskText);
     input.value = "";
-})
+    saveTasks();
+});
+    function addTask(text, done = false) {
+        const li = document.createElement ('li');
+        li.textContent = text;
+        if (done) li.classList.add ('done');
+    }
+    const delBtn = document.createElement('button');
+    delBtn.textContent = '❌'; 
+    delBtn.onclick = () => {
+        li.remove();
+        saveTasks();
+    };
+    li.onclick = () => {
+        li.classList.toggle('done');
+        saveTasks();
+    };
+    li.appendChild(delBtn);
+    taskList.appendChild(li);
+  function saveTasks() {
+    const tasks = [];
+    taskList.querySelectorAll('li').forEach(li => {
+        tasks.push({
+            text: li.firstChild.textContent,
+            done: li.classList.contains('done')
+        });
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  } ;
+  function loadTasks() {
+    const tasks = JSON.parse(localStorage,getItem('tasks')) 
+    || [];
+    tasks.forEach(task => addTask(task.text, task.done));
+  }
+    
